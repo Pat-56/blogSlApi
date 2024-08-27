e9272e32ee73dbf1fd22e4c08138aad2f2caffd5
Pat <patrick81.dev@gmail.com>
2024-08-27 20:45:38 +0000

    https ok

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import * as fs from 'fs';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+   const httpsOptions = {
+    key: fs.readFileSync('./.secrets/create-cert-key.pem'),
+    cert: fs.readFileSync('./.secrets/create-cert.pem'),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   await app.listen(3000, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();
